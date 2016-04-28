ea3f132f282986917fe98c4f4ac84b3165f88cd7
Lezli <ian.likhanga@strathmore.edu>
2016-04-28 13:32:30 -0700

    Header Footer and Sign up views

---
+++ application/views/footer.php
@@ -0,0 +1,12 @@
+
+<div class="row">
+    <div class="span12">
+        <div class="well">
+            <center><h3>Powered by Lezli</h3></</center>
+        </div>
+    </div>
+</div>    
+</div>
+</body>
+</html>
+
---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/bootstrap.css' ?>" />
+    </head>
+    <body>
+        <div class="container">
+            <div class ="row">
+                <div class ="span12">
+                    <div class ="well">
+                        <centre><h1> Share your thought's:... "Fungua Roho"</h1></center> 
+                    </div>
+                </div>
+             </div>
